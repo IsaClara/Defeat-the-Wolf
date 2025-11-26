c776c325941e26af344c3e3731aacf0749fce601
Isa Clara <isaclarabispo@gmail.com>
2025-11-25 21:34:57 -0300

    Primeira versão do jogo do lobo em C
    Jogo de combate em C onde o jogador enfrenta um lobo, usando ataques, defesa e poções de vida.

---
+++ DefeatTheWolf.c
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
+
+// VARIÁVEIS GLOBAIS //
+#define atkIn 10
+#define atkPlayer 5
+int opcao, pocao = 3;
+
+
+// CORES DO PROJETO //
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define GOLD    "\033[33m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+
+
+// STATUS DO INIMIGO //
+int hpIn = 30;
+char movimentoIn[150] = "O lobo te encara fixamente.";
+
+
+// STATUS DO PLAYER //
+int hpPlayer = 50;
+
+
+
+int main();
+void menu();
+void acaoInimigo();
+void atacarInimigo();
+void defender();
+void usarPocao();
+
+
+
+
+/////////////////////////////////// INIMIGO ///////////////////////////////////////
+// AÇÃO DO INIMIGO //
+void acaoInimigo(){
+	system("cls");
+
+    char localMsg[150];
+    snprintf(localMsg, sizeof(localMsg), "O lobo rosna, pronto para atacar.");
+    strncpy(movimentoIn, localMsg, sizeof(movimentoIn)-1);
+    movimentoIn[sizeof(movimentoIn)-1] = '\0';
+	// ASCII DO LOBO
+	printf("                        ,     ,\n");
+	printf("                        |\\---/|\n");
+	printf("                       /  , , |\n");
+	printf("                  __.-'|  / \\ /\n");
+	printf("         __ ___.-'        ._O|\n");
+	printf("      .-'  '        :      _/       " RED " %s" RESET "\n", movimentoIn);
+	printf("     / ,    .        .     |\n");
+	printf("    :  ;    :        :   _/\n");
+	printf("    |  |   .'     __:   /\n");
+	printf("    |  :   /'----'| \\  |\n");
+	printf("    \\  |\\  |      | /| |\n");
+	printf("     '.'| /       || \\ |\n");
+	printf("     | /|.'       '.l \\\\_\n");
+	printf("     || ||             '-'\n");
+	printf("     '-''-'\n\n\n");
+
+	getch();
+
+	int resultado = rand() % 2;
+
+	if (resultado == 0) {
+		printf(RED "Você é mordido pelo lobo.\n" RESET);
+		if (opcao == 2) {
+			printf("Você defendeu, você não perde vida!\n");
+		} else {
+			printf("Você perde vida!\n");
+			hpPlayer = hpPlayer - atkIn;
+		}
+	    getch();
+	}else {
+  	  	printf("O lobo não consegue te morder.");
+	    getch();
+	}
+
+}
+
+
+
+/////////////////////////////////// PLAYER ////////////////////////////////////////
+// ATACAR INIMIGO //
+void atacarInimigo(){
+	system("cls");
+
+	printf(GOLD "===========================================================\n" RESET);
+	printf(BOLD GREEN "Pressione qualquer tecla para ver se você consegue atacar!\n" RESET);
+	printf(GOLD "===========================================================\n" RESET);
+    getch();
+
+    int resultado = rand() % 2;
+
+	if (resultado == 0) {
+	    printf("Você acertou o ataque!\n");
+	    hpIn = hpIn - atkPlayer;
+	    getch();
+	} else {
+	    printf(RED "Você errou o ataque...\n" RESET);
+	    getch();
+	}
+}
+
+
+// DEFENDER //
+void defender(){
+	system("cls");
+
+	printf(GOLD "===========================================================\n" RESET);
+	printf(BOLD GREEN "Você passa essa rodada defendendo!\n" RESET);
+	printf(GOLD "============================================================\n" RESET);
+	getch();
+}
+
+
+// USAR POÇÃO //
+void usarPocao(){
+	system("cls");
+
+    if (pocao > 0) {
+        pocao--;
+        hpPlayer += 5;
+        if (hpPlayer > 50) hpPlayer = 50;
+        printf(GOLD "===========================================================\n" RESET);
+		printf(BOLD GREEN "Você usou uma poção e recuperou 5 HP!\n" RESET);
+    	printf(GOLD "===========================================================\n" RESET);
+        printf("Poções restantes: %d", pocao);
+        printf("\nHP agora: %d\n", hpPlayer);
+    } else {
+        printf("\nVocê não tem mais poções!\n");
+    }
+    getch();
+}
+
+
+
+/////////////////////////////////// MENU ////////////////////////////////////////
+void menu(){
+	system("cls");
+
+	// ASCII DO LOBO
+	printf("                        ,     ,\n");
+	printf("                        |\\---/|\n");
+	printf("                       /  , , |\n");
+	printf("                  __.-'|  / \\ /      HP: %d \n", hpIn);
+	printf("         __ ___.-'        ._O|\n");
+	printf("      .-'  '        :      _/       " RED " %s" RESET "\n", movimentoIn);
+	printf("     / ,    .        .     |\n");
+	printf("    :  ;    :        :   _/\n");
+	printf("    |  |   .'     __:   /\n");
+	printf("    |  :   /'----'| \\  |\n");
+	printf("    \\  |\\  |      | /| |\n");
+	printf("     '.'| /       || \\ |\n");
+	printf("     | /|.'       '.l \\\\_\n");
+	printf("     || ||             '-'\n");
+	printf("     '-''-'\n");
+
+
+	// STATUS DO JOGADOR
+	printf(GOLD "=====================================\n" RESET);
+	printf(BOLD GREEN "        STATUS DO AVENTUREIRO\n" RESET);
+	printf(GOLD "=====================================\n" RESET);
+
+	printf("       HP: %d\t\tATK: %d\n", hpPlayer, atkPlayer);
+
+	printf(GOLD "=====================================\n" RESET);
+
+	printf(BOLD GREEN "            MENU DE AÇÕES\n" RESET);
+	printf(GOLD "=====================================\n" RESET);
+
+	printf(GOLD "[1]" RESET  " Atacar o inimigo\n");
+	printf(GOLD "[2]" RESET " Defender\n");
+	printf(GOLD "[3]" RESET " Usar poção de vida (%d)\n", pocao);
+	printf(GOLD "[4]" RESET " Correr\n");
+
+	printf(GOLD "=====================================\n" RESET);
+
+	// === INPUT ===
+	printf("\nEscolha sua ação: ");
+	if (scanf("%d", &opcao) != 1) {
+	    while (getchar() != '\n');
+	    opcao = 0;
+	}
+
+   switch (opcao) {
+    case 1:
+		atacarInimigo();
+		break;
+    case 2:
+		defender();
+        break;
+	case 3:
+		usarPocao();
+		break;
+	case 4:
+        break;
+   default:
+		printf("\nEscolha inválida. Tente novamente.\n");
+        getch();
+        break;
+   }
+
+   if (opcao != 4 && hpIn > 0 && hpPlayer > 0) {
+        acaoInimigo();
+    }
+}
+
+
+
+/////////////////////////////////// MAIN ////////////////////////////////////////
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	srand((unsigned) time(NULL));
+	while (opcao != 4 && hpPlayer > 0 && hpIn > 0){
+		menu();
+	}
+	if(opcao == 4){
+		system("cls");
+		printf(BOLD GREEN"\nVocê correu do Lobo...\n" RESET);
+		getch();
+	} else if(hpPlayer <= 0){
+		system("cls");
+        printf(RED "Você foi derrotado pelo lobo...\n" RESET);
+        getch();
+	} else if (hpIn <= 0){
+		system("cls");
+        printf(GREEN BOLD "Você derrotou o lobo! Parabéns!\n" RESET);
+		getch();
+	}
+
+	printf(BOLD GREEN"\nFim do jogo, obrigado por jogar!\n\n" RESET);
+	printf("Criado por: Isa Clara");
+	return 0;
+}
